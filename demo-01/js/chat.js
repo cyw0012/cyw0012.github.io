66c6ba7b32743ad5fd9c36d9529d6f998fd81251
cyw0012 <104307074+cyw0012@users.noreply.github.com>
2022-12-16 09:08:53 +0800

    Update chat.js

--- demo-01/js/chat.js
+++ demo-01/js/chat.js
@@ -2,6 +2,8 @@ $(function () {
   // 初始化右侧滚动条(到底部)
   // 这个方法定义在scroll.js中
   resetui()
+  // 获取焦点
+  $(".input_txt")[0].focus()
 
   // 1.点击发送：进行渲染，发送请求获取聊天消息
   $("#btnSend").click(function () {
@@ -28,8 +30,11 @@ $(function () {
   function getMsg(text) {
     $.ajax({
       method: "GET",
-      url: "http://www.liulongbin.top:3006/api/robot",
+      // http://www.liulongbin.top:3006/api/robot
+      url: "https://api.ownthink.com/bot",
       data: {
+        appid: '56eeb709b0ccef9cbbc6bad68e556be5',
+        userid: 'mSaGBUjX',
         spoken: text,
       },
       success: function (res) {
@@ -37,14 +42,23 @@ $(function () {
           alert(res.message);
           return;
         }
-        console.log(res);
+        // console.log(res);
         // 接受聊天消息,渲染
+        let content = '';
         let msg = res.data.info.text
+        content += msg;
         $(".talk_list").append("<li class='left_word'><img src='img/pic1.png' /> <span>" + msg + "</span></li>");
+        msg = res.data.info.heuristic
+        if (msg) {
+          $(".talk_list").append("<li class='left_word'><img src='img/pic1.png' /> <span>" + msg + "</span></li>");
+          content += msg;
+        }
         resetui()
 
         // 文字转语音
-        getVoice(msg)
+        if ($('#check').prop('checked')) {
+          getVoice(content)
+        }
       }
     })
   }
@@ -58,7 +72,7 @@ $(function () {
         text: text,
       },
       success: function (res) {
-        console.log(res);
+        // console.log(res);
         if (res.status !== 200) {
           console.log(res.message);
           return;
